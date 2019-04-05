6db75bfe0d51b469a3579338c4acf39dc9cd9f6f
corenel <xxdsox@gmail.com>
2019-04-05 10:41:31 +0800

    Add packages
    - Support nvidia-docker
    - Support gnome terminal theme
    - Support wechat and qq

--- packages/install_theme.sh
+++ packages/install_theme.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository -y ppa:papirus/papirus
-sudo add-apt-repository -y ppa:noobslab/themes
-sudo find /etc/apt/sources.list.d/ -type f -name "*.list" -exec sed -i.bak -r 's#deb(-src)?\s*http(s)?://ppa.launchpad.net#deb\1 http\2://launchpad.proxy.ustclug.org#ig' {} \;
-sudo apt-get update
-sudo apt-get install -y unity-tweak-tool \
-  arc-flatabulous-theme \
-  papirus-folders papirus-icon-theme
+
+if [ -z $COMMON_SOURCED ]; then
+  source include/common.sh
+fi
+
+install_unity_theme() {
+  sudo add-apt-repository -y ppa:papirus/papirus
+  sudo add-apt-repository -y ppa:noobslab/themes
+  sudo find /etc/apt/sources.list.d/ -type f -name "*.list" -exec sed -i.bak -r 's#deb(-src)?\s*http(s)?://ppa.launchpad.net#deb\1 http\2://launchpad.proxy.ustclug.org#ig' {} \;
+  sudo apt-get update
+  sudo apt-get install -y \
+    unity-tweak-tool \
+    arc-flatabulous-theme \
+    papirus-folders papirus-icon-theme
+}
+
+install_gnome_terminal_theme() {
+  bash -c  "$(wget -qO- https://git.io/vQgMr)"
+}
+
+confirm install_unity_theme "Install theme and icons for Unity"
+confirm install_gnome_terminal_theme "Install theme for Gnome Terminal"
