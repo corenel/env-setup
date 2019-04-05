3a41a21d8ee7bd0c1f1d4dac2679767a0a32165c
corenel <xxdsox@gmail.com>
2019-04-05 10:46:08 +0800

    Add input method

---
+++ packages/install_input_methods.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+if [ -z $COMMON_SOURCED ]; then
+  source include/common.sh
+fi
+
+sudo apt-get install -y fcitx fcitx-googlepinyin fcitx-mozc ibus-qt4
+sudo apt remove -y fcitx-ui-qimpanel 
+
+status "Please switch 'keyboard input method system' to 'fcitx'"
+status "in 'System Settings' -> 'Language Support'"
