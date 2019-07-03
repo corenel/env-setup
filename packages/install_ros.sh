166a07d5bc8ff6783258765a7d63fdc6be511e89
corenel <xxdsox@gmail.com>
2019-07-03 20:47:51 +0800

    Add env varibale for ROS version

--- packages/install_ros.sh
+++ packages/install_ros.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
+export ZJUDANCER_ROS_DISTRO=kinetic
+
 # use USTC mirror for higher downloading speed
 sudo sh -c '. /etc/lsb-release && echo "deb https://mirrors.ustc.edu.cn/ros/ubuntu/ $DISTRIB_CODENAME main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt-get update
 sudo apt-get install -y \
-  ros-kinetic-ros-base \
+  ros-${ZJUDANCER_ROS_DISTRO}-ros-base \
   python-rosinstall python-rosinstall-generator python-wstool \
-  ros-kinetic-image-transport ros-kinetic-tf2 ros-kinetic-tf2-geometry-msgs
-# ros-kinetic-opencv3
+  ros-${ZJUDANCER_ROS_DISTRO}-image-transport \
+  ros-${ZJUDANCER_ROS_DISTRO}-tf2 \
+  ros-${ZJUDANCER_ROS_DISTRO}-tf2-geometry-msgs
 
 sudo rosdep init
 rosdep update
 
 # if you use bash, replace .zshrc with .bashrc
-# echo 'source /opt/ros/kinetic/setup.zsh' >> ~/.zshrc
+# echo 'source /opt/ros/${ZJUDANCER_ROS_DISTRO}/setup.zsh' >> ~/.zshrc
 # source ~/.zshrc
