071657ab02c56b0eea5af5c3e4a9b2ad6d8194e8
corenel <xxdsox@gmail.com>
2019-07-07 21:02:13 +0800

    Fix the installation of ROS

--- packages/install_ros.sh
+++ packages/install_ros.sh
@@ -4,13 +4,13 @@ if [ -z $COMMON_SOURCED ]; then
   source include/common.sh
 fi
 
-export ZJUDANCER_ROS_DISTRO=kinetic
+export ZJUDANCER_ROS_DISTRO=melodic
 prompt_default ZJUDANCER_ROS_DISTRO "ROS Distro [${ZJUDANCER_ROS_DISTRO}]"
 
 # use USTC mirror for higher downloading speed
 # sudo sh -c '. /etc/lsb-release && echo "deb https://mirrors.ustc.edu.cn/ros/ubuntu/ $DISTRIB_CODENAME main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
 sudo apt-get install -y \
   ros-${ZJUDANCER_ROS_DISTRO}-ros-base \
