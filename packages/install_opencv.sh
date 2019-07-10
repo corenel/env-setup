035a889054b9bf5a97d97796cd9c7d235e25ba37
corenel <xxdsox@gmail.com>
2019-07-10 20:24:05 +0800

    Update installation script of OpenCV

--- packages/install_opencv.sh
+++ packages/install_opencv.sh
@@ -5,11 +5,9 @@ if [ -z $COMMON_SOURCED ]; then
 fi
 
 OPENCV_VERSION=3.4.6
-CUDA_ARCH="6.2"
 TMP_DIR=/tmp
 
 prompt_default OPENCV_VERSION "OpenCV Version [${OPENCV_VERSION}]"
-prompt_default CUDA_ARCH "CUDA Arch [${CUDA_ARCH}]"
 
 status "Install dependencies of OpenCV"
 sudo apt-get install -yq \
@@ -48,8 +46,11 @@ fi
 status "Building OpenCV"
 cd opencv-${OPENCV_VERSION}
 mkdir -p build && cd build
-if [[ ${ZJUDANCER_GPU} == "1" ]]; then
-  echo 'Build OpenCV on Jetson Tegra with CUDA'
+
+install_opencv_embedded_gpu() {
+  echo 'Build OpenCV on Embedded with CUDA'
+  CUDA_ARCH="6.2"
+  prompt_default CUDA_ARCH "CUDA Arch [${CUDA_ARCH}]"
   cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=/usr \
@@ -90,8 +91,49 @@ if [[ ${ZJUDANCER_GPU} == "1" ]]; then
     .. && \
     make && \
     sudo make install
-elif [ -x "$(command -v nvcc)" ]; then
+}
+
+install_opencv_embedded_cpu() {
+  echo 'Build OpenCV on Embedded without CUDA'
+  cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DBUILD_PNG=OFF \
+    -DBUILD_TIFF=OFF \
+    -DBUILD_TBB=OFF \
+    -DBUILD_JPEG=OFF \
+    -DBUILD_JASPER=OFF \
+    -DBUILD_ZLIB=OFF \
+    -DBUILD_TESTS=OFF \
+    -DBUILD_PERF_TESTS=OFF \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_opencv_java=OFF \
+    -DBUILD_opencv_python2=ON \
+    -DBUILD_opencv_python3=ON \
+    -DENABLE_NEON=ON \
+    -DWITH_OPENCL=OFF \
+    -DWITH_OPENMP=OFF \
+    -DWITH_FFMPEG=ON \
+    -DWITH_GSTREAMER=ON \
+    -DWITH_LIBV4L=ON \
+    -DENABLE_FAST_MATH=1 \
+    -DWITH_GTK=ON \
+    -DWITH_VTK=OFF \
+    -DWITH_TBB=ON \
+    -DWITH_1394=OFF \
+    -DWITH_OPENEXR=OFF \
+    -DINSTALL_C_EXAMPLES=OFF \
+    -DINSTALL_PYTHON_EXAMPLES=OFF \
+    -DINSTALL_TESTS=OFF \
+    .. && \
+    make && \
+    sudo make install
+}
+
+install_opencv_desktop_gpu() {
   echo 'Build OpenCV on PC with CUDA'
+  CUDA_ARCH="6.2"
+  prompt_default CUDA_ARCH "CUDA Arch [${CUDA_ARCH}]"
   cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=/usr \
@@ -132,7 +174,9 @@ elif [ -x "$(command -v nvcc)" ]; then
     .. && \
     make && \
     sudo make install
-else
+}
+
+install_opencv_desktop_cpu() {
   echo 'Build OpenCV on PC without CUDA'
   cmake \
     -DCMAKE_BUILD_TYPE=Release \
@@ -167,6 +211,20 @@ else
     .. && \
     make && \
     sudo make install
+}
+
+if [[ "$(uname -m)" == "x86_64" ]]; then
+  if [ -x "$(command -v nvcc)" ]; then
+    confirm install_opencv_desktop_gpu "Install OpenCV on PC with CUDA"
+  else
+    confirm install_opencv_desktop_cpu "Install OpenCV on PC without CUDA"
+  fi
+else
+  if [ -x "$(command -v nvcc)" ]; then
+    confirm install_opencv_embedded_gpu "Install OpenCV on Embedded with CUDA"
+  else
+    confirm install_opencv_embedded_cpu "Install OpenCV on Embedded without CUDA"
+  fi
 fi
 
 popd
