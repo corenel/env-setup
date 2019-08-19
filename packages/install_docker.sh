dfb226557b5f4922d02745e1ead7dedfb1e72cce
corenel <xxdsox@gmail.com>
2019-08-19 15:01:54 +0800

    Support new nvidia-container-toolkit

--- packages/install_docker.sh
+++ packages/install_docker.sh
@@ -35,24 +35,28 @@ install_docker() {
 }
 
 install_nvidia_docker() {
-  # If you have nvidia-docker 1.0 installed: we need to remove it and all existing GPU containers
-  docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
-  sudo apt-get purge -y nvidia-docker
-
+  warning "Note that with the release of Docker 19.03, usage of nvidia-docker2 packages are deprecated since NVIDIA GPUs are now natively supported as devices in the Docker runtime. If you are an existing user of the nvidia-docker2 packages, use `update_with_nvidia_docker2` function."
   # Add the package repositories
-  curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
-    sudo apt-key add -
   distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-  curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
-    sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-  sudo apt-get update
+  curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+  curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
-  # Install nvidia-docker2 and reload the Docker daemon configuration
-  sudo apt-get install -y nvidia-docker2
-  sudo pkill -SIGHUP dockerd
+  # Install the package
+  sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+  sudo systemctl restart docker
 
   # Test nvidia-smi with the latest official CUDA image
-  docker run --runtime=nvidia --rm nvidia/cuda:9.0-base nvidia-smi
+  docker run --gpus all nvidia/cuda:9.0-base nvidia-smi
+}
+
+update_with_nvidia_docker2() {
+  # On debian based distributions: Ubuntu / Debian
+  sudo apt-get update
+  sudo apt-get --only-upgrade install docker-ce nvidia-docker2
+  sudo systemctl restart docker
+
+  # Test nvidia-smi with the latest official CUDA image
+  docker run --gpus all nvidia/cuda:9.0-base nvidia-smi
 }
 
 
