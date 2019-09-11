6f9c093896018feaac42e9f39161d51d32520a06
corenel <xxdsox@gmail.com>
2019-09-11 14:01:00 +0800

    Add installation script for FFmpeg

---
+++ packages/install_ffmpeg.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+if [ -z $COMMON_SOURCED ]; then
+  source include/common.sh
+fi
+
+TMP_DIR=/tmp
+
+# purge installed ffmpeg
+if confirmation "Purge installed ffmpeg"; then
+  sudo apt remove --purge --auto-remove ffmpeg
+fi
+
+# install dependencies
+sudo apt-get install yasm libx264-dev libx264-dev \
+  libvpx-dev libfdk-aac-dev libmp3lame-dev libopus-dev
+
+# get latest ffmpeg source
+pushd ${TMP_DIR}
+# git clone https://git.ffmpeg.org/ffmpeg.git
+wget -O ffmpeg-snapshot.tar.bz2 \
+  https://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2 && \
+  tar xjvf ffmpeg-snapshot.tar.bz2
+
+# build ffmpeg
+cd ffmpeg
+if [ -x "$(command -v nvcc)" ]; then
+  git clone git@github.com:FFmpeg/nv-codec-headers.git && \
+    cd nv-codec-headers && \
+    sudo make install && \
+    cd ..
+  ./configure \
+    --enable-gpl \
+    --enable-libaom \
+    --enable-libass \
+    --enable-libfdk-aac \
+    --enable-libfreetype \
+    --enable-libmp3lame \
+    --enable-libopus \
+    --enable-libvorbis \
+    --enable-libvpx \
+    --enable-libx264 \
+    --enable-libx265 \
+    --enable-nonfree \
+    --enable-cuda \
+    --enable-cuvid \
+    --enable-nvenc \
+    --enable-nonfree \
+    --enable-libnpp \
+    --extra-cflags=-I/usr/local/cuda/include \
+    --extra-ldflags=-L/usr/local/cuda/lib64 && \
+    make -j -s && \
+    sudo make install
+else
+  ./configure \
+    --enable-gpl \
+    --enable-libaom \
+    --enable-libass \
+    --enable-libfdk-aac \
+    --enable-libfreetype \
+    --enable-libmp3lame \
+    --enable-libopus \
+    --enable-libvorbis \
+    --enable-libvpx \
+    --enable-libx264 \
+    --enable-libx265 \
+    --enable-nonfree && \
+    make -j -s && \
+    sudo make install
+fi
+
+# show ffmpeg version
+ffmpeg -version
+
+popd
