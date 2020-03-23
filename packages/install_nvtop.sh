57584e74fc66e6edc8bac178a5ff467eb0517410
corenel <xxdsox@gmail.com>
2020-03-23 10:39:07 +0800

    feat(packages): Add builidng script for nvtop

---
+++ packages/install_nvtop.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+if [ -z $COMMON_SOURCED ]; then
+  source include/common.sh
+fi
+
+TMP_DIR=/tmp
+
+# install dependencies
+# sudo apt install -y cmake libncurses5-dev libncursesw5-dev git
+sudo apt install -y libncurses5-dev libncursesw5-dev git
+
+# get latest source
+pushd ${TMP_DIR}
+git clone https://github.com/Syllo/nvtop.git
+
+# build cmake
+mkdir -p nvtop/build && cd nvtop/build
+cmake ..
+make
+sudo make install
+
+# show
+nvtop
+
+popd
+
