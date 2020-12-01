87a8d70a157bb688775e78cb2e8746cc2c8b51e6
corenel <xxdsox@gmail.com>
2020-12-01 21:43:19 +0800

    fix(packages): update docker installtion script

--- packages/install_docker.sh
+++ packages/install_docker.sh
@@ -28,6 +28,9 @@ install_docker() {
   # test hello-world
   # sudo docker run hello-world
 
+  sudo systemctl start docker
+  sudo systemctl enable docker
+
   # non-root user
   sudo groupadd docker
   sudo usermod -aG docker $USER
@@ -38,16 +41,16 @@ install_docker() {
 install_nvidia_docker() {
   warning "Note that with the release of Docker 19.03, usage of nvidia-docker2 packages are deprecated since NVIDIA GPUs are now natively supported as devices in the Docker runtime. If you are an existing user of the nvidia-docker2 packages, use `update_with_nvidia_docker2` function."
   # Add the package repositories
-  distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-  curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-  curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+  distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
   # Install the package
-  sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+  sudo apt-get update && sudo apt-get install -y nvidia-docker2
   sudo systemctl restart docker
 
   # Test nvidia-smi with the latest official CUDA image
-  docker run --gpus all nvidia/cuda:9.0-base nvidia-smi
+  docker run --gpus all nvidia/cuda:11.0-base nvidia-smi
 }
 
 update_with_nvidia_docker2() {
@@ -59,7 +62,6 @@ update_with_nvidia_docker2() {
   # Test nvidia-smi with the latest official CUDA image
   docker run --gpus all nvidia/cuda:9.0-base nvidia-smi
 }
-
 
 confirm install_docker "Install docker"
 confirm install_nvidia_docker "Install nvidia-docker"
